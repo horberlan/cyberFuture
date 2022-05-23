d27235e44054ec697f2e40d8707f93e14a533af9
neo0112358 <borjaruizreverter@gmail.com>
2022-05-23 15:01:47 -0300

    Formating document

--- src/examples/App.js
+++ src/examples/App.js
@@ -1,40 +1,61 @@
-import React from 'react';
-import { Button, Card,CardCarousel } from '../lib';
-import styled from 'styled-components'
-
+import React from "react";
+import { Button, Card, CardCarousel } from "../lib";
+import styled from "styled-components";
 
 const Flex = styled.div`
-display:flex;
-flex-direction:row;
-flex-wrap:wrap;
-gap:5%;
-position:relative;
-margin: 0px auto;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 5%;
+  position: relative;
+  margin: 0px auto;
 `;
-
 
 const App = () => (
   <div>
     <Flex>
-      <Button outline text='CyberFuture' variant='Matrix' color='matrix' />
-      <Button text='Getelementbyid' variant='OutlinedCyberRose' type="outlined" />
-      <Button text='Lane Art Design' variant='Rose' />
-      <Button text='Button' variant='Punk' />
-      <Button text='Button' variant='Gold' />
-      <Button text='Button' variant='OutlinedCyberSly' />
+      <Button outline text="CyberFuture" variant="Matrix" color="matrix" />
+      <Button
+        text="Getelementbyid"
+        variant="OutlinedCyberRose"
+        type="outlined"
+      />
+      <Button text="Lane Art Design" variant="Rose" />
+      <Button text="Button" variant="Punk" />
+      <Button text="Button" variant="Gold" />
+      <Button text="Button" variant="OutlinedCyberSly" />
     </Flex>
     <Flex>
-    <Card variant='matrix' title="Card Title" body='some content' end={<Button text='button' variant='Matrix' />}>
-    </Card>
-    <CardCarousel variant='rose' title="Card Title" body='some content' end={<Button text='button' variant='Rose' />}>
-    </CardCarousel>
-    <Card variant='rose' title="Card Title" body='some content' end={<Button text='button' variant='Rose' />}>
-    </Card>
-    
-    <Card variant='rose' title="Card Title" body='some content' end={<Button text='button' variant='Rose' />}>
-    </Card>
-    <Card variant='matrix' title="Card Title" body={<div>content</div> } end={<Button text='button' variant='Matrix' />}>
-    </Card>
+      <Card
+        variant="matrix"
+        title="Card Title"
+        body="some content"
+        end={<Button text="button" variant="Matrix" />}
+      ></Card>
+      <CardCarousel
+        variant="rose"
+        title="Card Title"
+        body="some content"
+        end={<Button text="button" variant="Rose" />}
+      ></CardCarousel>
+      <Card
+        variant="rose"
+        title="Card Title"
+        body="some content"
+        end={<Button text="button" variant="Rose" />}
+      ></Card>
+      <Card
+        variant="rose"
+        title="Card Title"
+        body="some content"
+        end={<Button text="button" variant="Rose" />}
+      ></Card>
+      <Card
+        variant="matrix"
+        title="Card Title"
+        body={<div>content</div>}
+        end={<Button text="button" variant="Matrix" />}
+      ></Card>
     </Flex>
   </div>
 );

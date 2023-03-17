380142ce06e9674fc810d4c2e114c98e0cb2ec7f
horberlan <horberlan@gmail.com>
2023-03-16 23:24:42 -0300

    hotfix: change build type

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,31 @@
+const path = require("path");
+
 module.exports = {
+  mode: "production",
+  entry: "/src/lib/index.ts",
+  output: {
+    path: path.resolve("dist"),
+    filename: "index.ts",
+  },
   module: {
     rules: [
       {
         test: /\\.(png|jp(e*)g|svg|gif)$/,
         use: ["file-loader"],
       },
+      {
+        test: /\.([cm]?ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "ts-loader",
+          options: {
+            projectReferences: true,
+          },
+        },
+      },
     ],
   },
   resolve: {
-    extensions: [".js", ".jsx"],
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
   },
 };
